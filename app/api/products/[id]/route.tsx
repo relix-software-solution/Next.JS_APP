1260833604bbad0e366382ae395b548d0d69d3f9
MoShero-13 <mo3206213@gmail.com>
2025-09-29 15:32:53 +0300

    Exercise: Building Products API

---
+++ app/api/products/[id]/route.tsx
@@ -0,0 +1,49 @@
+import { NextRequest, NextResponse } from "next/server";
+import schema from "../../users/schema";
+
+export function GET(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch data from a db
+  // If not found, return 404 error
+  // Else return data
+
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({ id: 1, name: "Mosh" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  // If invalid, return 400
+  const body = await request.json();
+  const validation = schema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.issues, { status: 400 });
+  // Fetch the user with the given id
+  // If doesn't exist, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Update the user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
+}
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch user from db
+  // If not found, return 404
+  //  Delete the user
+  // Return 200
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({});
+}
