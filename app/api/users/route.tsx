2b56159398b90860eecc38cad997fb72bbe38181
MoShero-13 <mo3206213@gmail.com>
2025-09-29 14:56:01 +0300

    Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -6,3 +6,12 @@ export function GET(request: NextRequest) {
     { id: 2, name: "John" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validate
+  // If invalid, return 400
+  // Else, return
+  if (!body.name) return NextResponse.json({ error: "Name is required" });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
