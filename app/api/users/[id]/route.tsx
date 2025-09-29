f7ee4baf9913b09dcb80d9526683c62438bdd7f6
MoShero-13 <mo3206213@gmail.com>
2025-09-29 15:10:39 +0300

    Deleting an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -32,3 +32,17 @@ export async function PUT(
   // Return the updated user
   return NextResponse.json({ id: 1, name: body.name });
 }
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Fetch user from db
+  // If not found, return 404
+  //  Delete the user
+  // Return 200
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+
+  return NextResponse.json({});
+}
