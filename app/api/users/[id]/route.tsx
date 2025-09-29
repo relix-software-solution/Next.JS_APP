970345a9a59af2894fd7db791460c98f8b9ecc91
MoShero-13 <mo3206213@gmail.com>
2025-09-29 15:06:09 +0300

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,3 +1,4 @@
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
 export function GET(
@@ -12,4 +13,22 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
 
   return NextResponse.json({ id: 1, name: "Mosh" });
+}
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  // If invalid, return 400
+  const body = await request.json();
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // Fetch the user with the given id
+  // If doesn't exist, return 404
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Update the user
+  // Return the updated user
+  return NextResponse.json({ id: 1, name: body.name });
 }
