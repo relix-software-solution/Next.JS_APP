9750940a766cefe72853746ea1d2e18e300fae9f
MoShero-13 <mo3206213@gmail.com>
2025-10-05 10:44:55 +0300

    Creating a Prisma Client

---
+++ prisma/client.ts
@@ -0,0 +1,20 @@
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client/extension";
+
+// Extend the global object with a PrismaClient instance in development
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
