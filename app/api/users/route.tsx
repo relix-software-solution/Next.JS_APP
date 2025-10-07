c12583f24ddb85c8835aedc088370276eacce9b9
MoShero-13 <mo3206213@gmail.com>
2025-10-07 09:59:32 +0300

    Creating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -14,6 +14,21 @@ export async function POST(request: NextRequest) {
   // If invalid, return 400
   // Else, return
   const validation = schema.safeParse(body);
-  if (!validation.success) return NextResponse.json(validation.error.issues);
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  if (!validation.success)
+    return NextResponse.json(validation.error.issues, { status: 400 });
+
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(newUser, { status: 201 });
 }
